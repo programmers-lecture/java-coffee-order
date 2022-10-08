d654832d4b1a2433f4afc0324d39ba345c7c1c9c
preferkim <shineecard5@gmail.com>
2022-10-08 17:31:33 +0900

    test: 방문이 처음인 고객의 쿠폰 개수 확인

--- src/test/java/coffee/order/service/CouponServiceTest.java
+++ src/test/java/coffee/order/service/CouponServiceTest.java
@@ -1,14 +1,24 @@
 package coffee.order.service;
 
+import coffee.order.view.model.PhoneNumber;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CouponServiceTest {
     private final CouponService couponService = new CouponService();
 
     @Test
-    void getCouponQuantity() {
+    void should_couponQuantityIsZero_for_newCustomer() {
+        // given
+        PhoneNumber phoneNumber = new PhoneNumber("000-0000-0001");
 
+        // when
+        Integer couponQuantity = couponService.getCouponQuantity(phoneNumber);
+
+        // then
+        assertThat(couponQuantity).isEqualTo(1);
     }
 }
