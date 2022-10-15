875b5cb687e67107d374336045ade182dcad2b05
hyena0608 <goatsfish@gmail.com>
2022-10-15 19:11:02 +0900

    chore

--- src/main/java/coffee/order/domain/kiosk/barista/factory/KioskBaristaProcess.java
+++
@@ -1,8 +0,0 @@
-package coffee.order.domain.kiosk.barista.factory;
-
-import coffee.order.domain.kiosk.KioskManager;
-
-public interface KioskBaristaProcess {
-
-    void process(KioskManager kioskManager);
-}
--- src/test/java/coffee/order/domain/kiosk/KioskCouponTest.java
+++
@@ -1,5 +0,0 @@
-package coffee.order.domain.kiosk;
-
-class KioskCouponTest {
-
-}
