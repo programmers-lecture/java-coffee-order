8353f15fa1235ec001c1ba1c84c1c18d830482c7
astrocat9311 <astrocat9311@gmail.com>
2022-11-17 00:14:01 +0900

    - 메뉴 객체 구현해보기

--- src/main/java/coffee/order/Main.java
+++ src/main/java/coffee/order/Main.java
@@ -2,6 +2,19 @@ package coffee.order;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        class Menu {
+            String number;
+            String name;
+            Integer price;
+
+            Menu(String number, String name, Integer price) {
+                this.number = number;
+                this.name = name;
+                this.price = price;
+            }
+
+        }
+
+        Menu coffee = new Menu("1", "Coffee", 1000);
     }
 }
