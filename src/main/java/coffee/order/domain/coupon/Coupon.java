ade73bd83d9baa7c813ae949ec7b14d77bdf4416
이배기 <a2000000@jonahyeon-ui-MacBookPro.local>
2023-01-03 18:45:58 +0900

    쿠폰 로직 구현

--- src/main/java/coffee/order/domain/coupon/Coupon.java
+++ src/main/java/coffee/order/domain/coupon/Coupon.java
@@ -1,4 +1,59 @@
 package coffee.order.domain.coupon;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 public class Coupon {
+    private final HashMap<String,Integer> coupon;
+
+    public Coupon(){
+        coupon = new HashMap<>();
+        coupon.put("010-1234-1234",10);
+        coupon.put("010-0000-0001",8);
+        coupon.put("010-5151-5252",4);
+        coupon.put("010-4242-6464",20);
+    }
+
+    // 휴대폰 번호의 정규식 검사로 유저 정보를 반환
+    public Map.Entry<String, Integer> findByPhoneNumber(String phoneNumber){
+        String customer = regexPhoneNUmber(phoneNumber);
+        return coupon.entrySet().stream()
+                .filter(m -> m.getKey().equals(customer))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("회원 정보가 없습니다."));
+    }
+
+    // 휴대폰 번호 정규식 검사
+    public String regexPhoneNUmber(String phoneNumber){
+        String regex = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$";
+        if(!Pattern.matches(regex, phoneNumber)){
+            throw new IllegalArgumentException("전화번호 형식을 맞춰주십쇼");
+        }
+        return phoneNumber;
+    }
+
+    // 쿠폰 조회
+    public int getCoupon(String phoneNUmber){
+        return coupon.get(findByPhoneNumber(phoneNUmber));
+    }
+
+    // 쿠폰 사용
+    public void useCoupon(String phoneNumber){
+        int couponNumber = getCoupon(phoneNumber);
+        if (couponNumber>=10){
+            int remain = couponNumber - 10;
+            coupon.replace(phoneNumber, remain);
+        }
+        throw new IllegalArgumentException("쿠폰 개수가 부족합니다.");
+    }
+
+    // 쿠폰 적립
+    public void addCoupon(String phoneNumber, int count){
+        int add = getCoupon(phoneNumber) + count;
+        coupon.replace(phoneNumber, add);
+    }
+
+
+
 }
--- src/test/java/MenuTest.java
+++ src/test/java/MenuTest.java
@@ -1,3 +1,4 @@
+import coffee.order.domain.coupon.Customer;
 import coffee.order.domain.meals.Menu;
 import coffee.order.domain.meals.Tea;
 import org.junit.jupiter.api.Test;
@@ -12,4 +13,13 @@ public class MenuTest {
         System.out.println(res.toString());
     }
 
+    @Test
+    public void 휴대폰번호_정규식_테스트(){
+        String n1 = "010-1234-1234";
+        String n2 = "01";
+//        Customer customer = new Customer(n1);
+//        Customer customer2 = new Customer(n2);
+//        System.out.println(customer.regexPhoneNUmber(n1));
+//        System.out.println(customer2.regexPhoneNUmber(n2));
+    }
 }
